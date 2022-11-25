a9ad3c24b1eb47ab14ed6bdaa6bfe9198c71ebdf
alancarrillo <alang.carrillo@alumnos.ugd.mx>
2022-11-24 21:36:37 -0600

    quitar inicio de sesion

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -61,12 +61,6 @@
                             agenda tu cita aqui
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link page-scroll d-flex flex-row align-items-center text-primary" href="/dashboard">
-                            <em data-feather="layout" width="18" height="18" class="mr-2"></em>
-                            iniciar sesion 
-                        </a>
-                    </li>
                 </ul>
             </div>
         </nav>
